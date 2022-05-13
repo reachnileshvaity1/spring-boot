5b918c3ffe083444ff23a663435a88277b734354
student <nilesh.vaity@netmagicsolutions.com>
2022-05-13 05:40:06 +0000

    commiting spring app

---
+++ src/main/java/demoproject/demosampleproject/DemoSampleProjectApplication.java
@@ -0,0 +1,13 @@
+package demoproject.demosampleproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSampleProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSampleProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/demoproject/demosampleproject/DemoSampleProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package demoproject.demosampleproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSampleProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
